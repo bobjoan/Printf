649d91e4129ebe23d84c39dc72de8d5ffad773d2
bobjoan <bobjoan966@gmail.com>
2023-06-27 08:33:47 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -2,22 +2,22 @@
 #include <stdarg.h>
 #include "main.h"
 /**
- * _printf - produces output according to a format
+ * _printf -  produces output according to a format
  * @format: character string
- * Return: number of characters printed 
+ * Return: number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, count = 0;
-	
+
 	va_start(args, format);
-	
+
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
+			i++;
 			if (format[i] == 'c')
 			{
 				putchar(va_arg(args, int));
